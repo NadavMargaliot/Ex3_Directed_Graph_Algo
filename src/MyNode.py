77f7bb2d123190d86e6bfd733fe34111ae3f9131
Adiel1892 <adiel1892@gmail.com>
2021-12-19 09:33:05 +0200

    starting

--- src/MyNode.py
+++ src/MyNode.py
@@ -1,8 +1,15 @@
 import MyPoint3d
 
 class MyNode:
-    def __init__(self ,key:int ,location:MyPoint3d):
-        self.key = key
-        self.location = location
+    def __init__(self ,id :int ,tag : int = 0, location: tuple = None):
+        self.id = id
+        self.tag = tag;
+        if location is not  None:
+            self.location = location
+        else:
+            self.location = (0,0,0)
+        self.info = ""
+        self.neighbors = {}
+        self.edges = {}
 
 
