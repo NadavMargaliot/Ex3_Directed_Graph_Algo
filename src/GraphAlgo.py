1961f994b248404bb770f4ab036c7d67c7366705
Adiel1892 <adiel1892@gmail.com>
2021-12-21 14:02:35 +0200

    GraphAlgo - shortest path

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,5 +1,7 @@
+import heapq
 import json
 from typing import List
+from numpy import inf
 
 from GraphAlgoInterface import GraphAlgoInterface
 from DiGraph import DiGraph
@@ -79,30 +81,69 @@ class GraphAlgo(GraphAlgoInterface):
             json.dump(curr_graph, json_file)
         return True
 
+
+
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         """
         Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
         @param id1: The start node id
         @param id2: The end node id
-        @return: The distance of the path, a list of the nodes ids that the path goes through
-        Example:
-#      >>> from GraphAlgo import GraphAlgo
-#       >>> g_algo = GraphAlgo()
-#        >>> g_algo.addNode(0)
-#        >>> g_algo.addNode(1)
-#        >>> g_algo.addNode(2)
-#        >>> g_algo.addEdge(0,1,1)
-#        >>> g_algo.addEdge(1,2,4)
-#        >>> g_algo.shortestPath(0,1)
-#        (1, [0, 1])
-#        >>> g_algo.shortestPath(0,2)
-#        (5, [0, 1, 2])
-        Notes:
-        If there is no path between id1 and id2, or one of them dose not exist the function returns (float('inf'),[])
-        More info:
-        https://en.wikipedia.org/wiki/Dijkstra's_algorithm
-        """
-        raise NotImplementedError
+        @return: The distance of the path, a list of the nodes ids that the path goes through"""
+        """initialize all weights of nodes to infinity"""
+        dist_weight = {node: inf for node in self.graph.nodes.keys()}
+        """keeping track on the shortest path to the nodes"""
+        previous_nodes = {id1: -1}
+        dist_weight[id1] = 0
+        queue = []
+        heapq.heappush(queue, (0, id1))
+        while queue:
+            current_node = heapq.heappop(queue)[1]
+            if dist_weight[current_node] == inf:
+                break
+            """iterating on the neighbors of the current node as pairs (neighbor = id , weight = weight)"""
+            for neighbour, weight in self.graph.nodes.get(current_node).neighbors_out.items():
+                alternative_route = dist_weight[current_node] + weight
+                if alternative_route < dist_weight[neighbour]:
+                    dist_weight[neighbour] = alternative_route
+                    previous_nodes[neighbour] = current_node
+                    """adding to the queue the distance to the neighbor and the neighbor as a pair
+                    the queue is a priority queue so when it will pop an node, it will pop the node
+                     with the smallest dist_weight"""
+                    heapq.heappush(queue, (dist_weight[neighbour], neighbour))
+                if current_node == id2:
+                    break
+        path = []
+        current_node = id2
+        if dist_weight[id2] == inf:
+            """there isn't a path from id1 to id2"""
+            return inf, []
+        while current_node != -1:
+            path.insert(0, current_node)
+            """shortest path"""
+            current_node = previous_nodes[current_node]
+
+        return dist_weight[id2], path
+        # if self.graph.nodes.get(id1) is None:
+        #     raise Exception('Node {} is not exist in the graph'.format(id1))
+        # if self.graph.nodes.get(id2) is None:
+        #     raise Exception('Node {} is not exist in the graph'.format(id2))
+        # if id1 == id2:
+        #     return 0 , [id1]
+        #
+        # heapq.heappush(self.graph.nodes.get(id1))
+        # while heapq:
+        #     tmp_node = heapq.heappop()
+        #     if tmp_node.info != "visited":
+        #         tmp_node.info = "visited"
+        #     for i in tmp_node.neighbors_out.keys:
+        #         if i.info != "visited":
+        #             if tmp_node.weight + i.weight < self.graph.nodes.get(i).weight:
+        #                 self.graph.nodes.get(i).setWeight(tmp_node.weight + i.weight)
+        #                 heapq.heappush(self.graph.nodes.get(i))
+        #
+        #
+
+
 
     def TSP(self, node_lst: List[int]) -> (List[int], float):
         """
--- src/TestGraphAlgo.py
+++ src/TestGraphAlgo.py
@@ -5,7 +5,6 @@ from src.MyNode import MyNode
 from GraphAlgo import GraphAlgo
 
 
-
 class MyTestCase(unittest.TestCase):
     def test_save_to_json(self):
         v1 = MyNode(1, None)
@@ -20,8 +19,6 @@ class MyTestCase(unittest.TestCase):
         g = GraphAlgo(graph)
         g.save_to_json("jsonTest")
 
-
-
     def test_load_from_json(self):
         v1 = MyNode(1, None)
         v2 = MyNode(2, None)
@@ -34,19 +31,41 @@ class MyTestCase(unittest.TestCase):
         graph.add_edge(1, 3, 5)
         alg = GraphAlgo(graph)
 
-
         alg.load_from_json("/Users/adielbenmeir/PycharmProjects/Ex3_Directed_Graph_Algo/src/jsonTest")
-        self.assertEqual(2 , alg.get_graph().e_size())
-        self.assertEqual(3 , alg.get_graph().v_size())
+        self.assertEqual(2, alg.get_graph().e_size())
+        self.assertEqual(3, alg.get_graph().v_size())
 
-
-
-
-
-
+    def test_shortest_path(self):
+        graph = DiGraph()
+        a = MyNode(1, None)
+        b = MyNode(2, None)
+        c = MyNode(3, None)
+        d = MyNode(4, None)
+        e = MyNode(5, None)
+        f = MyNode(6, None)
+        g = MyNode(7, None)
+        graph.add_node(a.id,a.location)
+        graph.add_node(b.id, b.location)
+        graph.add_node(c.id, c.location)
+        graph.add_node(d.id, d.location)
+        graph.add_node(e.id, e.location)
+        graph.add_node(f.id, f.location)
+        graph.add_node(g.id, g.location)
+        graph.add_edge(a.id,c.id,3)
+        graph.add_edge(a.id,f.id,2)
+        graph.add_edge(f.id,c.id,2)
+        graph.add_edge(c.id,d.id,4)
+        graph.add_edge(c.id,e.id,1)
+        graph.add_edge(f.id,e.id,3)
+        graph.add_edge(e.id,b.id,2)
+        graph.add_edge(f.id,b.id,6)
+        graph.add_edge(f.id,g.id,5)
+        graph.add_edge(g.id,b.id,2)
+        graph.add_edge(d.id,b.id,1)
+        alg = GraphAlgo(graph)
+        self.assertEqual((6,[1,3,5,2]),alg.shortest_path(a.id,b.id))
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
